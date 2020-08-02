040da3fcf798c515381cc9818107c62bd8ca3603
Gor <7495500@gmail.com>
2020-08-03 00:53:20 +0300

    Style change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_133817) do
+ActiveRecord::Schema.define(version: 2020_08_02_155447) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "email"
