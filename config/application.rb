ae5ed28ae33eb43e6e4ab5264ac5d08c9b17a9cd
Gor <7495500@gmail.com>
2023-03-10 18:39:13 +0300

    Modify app configure for deploy 5

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Delper6
+module DelperNew
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
