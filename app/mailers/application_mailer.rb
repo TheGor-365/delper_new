6af0aa6234bf0916138299ee30ff95542843a516
Gor <7495500@gmail.com>
2023-03-10 19:37:15 +0300

    Modify app configure for deploy 9

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: '7495500@gmail.com'
-
-  def send_welcome_email(user)
-    @user = user
-    mail(:to => @user.email, :subject => "Welcome!")
-  end
-
-  layout 'mailer'
+  # default from: '7495500@gmail.com'
+  #
+  # def send_welcome_email(user)
+  #   @user = user
+  #   mail(:to => @user.email, :subject => "Welcome!")
+  # end
+  #
+  # layout 'mailer'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
 User.create([
-  { username: 'sarah',    email: 'sarah@example.com',     password: 'Password1', user_role: 'admin' },
+  { username: 'sarah',    email: 'sarah@example.com',     password: 'Password1' },
   { username: 'emily',    email: 'emily@example.com',     password: 'Password1' },
   { username: 'melanie',  email: 'melanie@example.com',   password: 'Password1' },
   { username: 'caroline', email: 'caroline@example.com',  password: 'Password1' },
